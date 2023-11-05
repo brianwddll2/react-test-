e32410b036d28a7e35de97df131d1df6c05d8d74
Brian Waddell <brianwddll2@gmail.com>
2023-11-05 15:48:26 -0600

    Added functionailty and Css styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 
-import './App.css';
+import React, {useState} from 'react'; 
+import Tweet from './Tweet'
 
-function App() {
+
+
+
+function App(){
+ const [users, setUsers]= useState([
+  {name: "Ed", message:"Hello There", likes:"403"},
+  {name: "john", message:"I am awesome", likes:"504"}
+ ])
+  const [isRed, setRed] = useState(true);
+  const [count, setCount] = useState(0);
+ const increment = () => {
+  setCount(count+1);
+  setRed(isRed)
+ 
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-         Hello Welcome Brian Waddell 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <div className='App'>
+     <h1 className= {isRed ? 'red': 'yellow'}> Change my Color </h1>
+     <button onClick={(increment)}> Increment</button>
+     <h1> {count}</h1>
+     <Tweet name="Brian" message="I like snow" likes="4425"/> 
+     {users.map(user => (
+      <Tweet name={user.name} message={user.message} likes={user.likes} /> 
+     ))}
+     
+     
+
+     
+     
+      
+      
+      </div> 
+   
+   
   );
 }
-
 export default App;
